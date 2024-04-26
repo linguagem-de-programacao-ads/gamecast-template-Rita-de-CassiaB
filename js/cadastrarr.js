9c5df5808f2e9f9e48ef04e8bceb2dc5eef19423
Rita-de-CassiaB <Rita.Barbosa@sptech.school>
2024-04-25 21:25:56 -0300

    Commit da aula

---
+++ js/cadastrarr.js
@@ -0,0 +1,33 @@
+function cadastrarAgenda() {
+  console.log("Passei aqui")  
+}
+
+async function cadastrarAgenda(){
+    const nomeDigitado = document.getElementById("nome_jogo").value;
+    const dataDigitada = document.getElementById("data_jogo").value;
+    const descricaoDigitada = document.getElementById("descricao").value;
+    const gamersDigitados = document.getElementById("gamers").value;
+    const urlImgDigitada = document.getElementById("url_imagem").value;
+    const urlLiveDigitada = document.getElementById("url_live").value;
+
+    const dadosAgenda = {       
+        "nome": nomeDigitado, 
+        "dataJogo": dataDigitada,
+        "descricao": descricaoDigitada,
+        "gamers": gamersDigitados.split(','),
+        "urlAssistir": urlLiveDigitada,
+        "urlImagem": urlImgDigitada,
+    }
+
+    const respostaCadastro = await fetch("https://660f44b3356b87a55c510e2a.mockapi.io/agendas", {
+        method: "POST",
+        body: JSON.stringify(dadosAgenda),
+        headers: {"Content-type": "application/json; charset=UTF-8"}
+    });
+
+    if(respostaCadastro.status == 201){
+        window.location.href = "agenda.html";
+    }else{
+        alert("Ocorreu um erro ao cadastrar")
+    }
+}
